baf928c15cd74814af74011af93fac78728ce137
vanillaGCC <n.youshow68@gmail.com>
2023-11-08 16:42:00 +0900

    d

--- MobileOrder/src/main/java/com/example/mobileorder/Item/Item.java
+++ MobileOrder/src/main/java/com/example/mobileorder/Item/Item.java
@@ -3,53 +3,27 @@ import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+
 public class Item {
+    private final SimpleIntegerProperty code;
     private final SimpleStringProperty name;
     private final SimpleDoubleProperty price;
-    private final SimpleIntegerProperty quantity;
+    private SimpleIntegerProperty quantity;
 
-    public Item(String name, double price, int quantity) {
+    public Item(int code, String name, double price, int quantity) {
+        this.code = new SimpleIntegerProperty(code);
         this.name = new SimpleStringProperty(name);
         this.price = new SimpleDoubleProperty(price);
         this.quantity = new SimpleIntegerProperty(quantity);
     }
 
-    // nameのgetterとsetter
-    public String getName() {
-        return name.get();
-    }
+    public int getCode() { return code.get(); }
+    public void setCode(int fCode) { code.set(fCode); }
+    public String getName() { return name.get(); }
+    public void setName(String fName) { name.set(fName); }
+    public double getPrice() { return price.get(); }
+    public void setPrice(double fPrice) { price.set(fPrice); }
+    public int getQuantity() { return quantity.get(); }
+    public void setQuantity(int fQuantity) { quantity.set(fQuantity); }
 
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
-    public SimpleStringProperty nameProperty() {
-        return name;
-    }
-
-    // priceのgetterとsetter
-    public double getPrice() {
-        return price.get();
-    }
-
-    public void setPrice(double price) {
-        this.price.set(price);
-    }
-
-    public SimpleDoubleProperty priceProperty() {
-        return price;
-    }
-
-    // quantityのgetterとsetter
-    public int getQuantity() {
-        return quantity.get();
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity.set(quantity);
-    }
-
-    public SimpleIntegerProperty quantityProperty() {
-        return quantity;
-    }
 }
--- MobileOrder/src/main/java/com/example/mobileorder/controller/maincontroller.java
+++ MobileOrder/src/main/java/com/example/mobileorder/controller/maincontroller.java
@@ -1,116 +1,110 @@
 package com.example.mobileorder.controller;
-import com.example.mobileorder.Item.*;
-import com.example.mobileorder.connectionToDB;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Button;
-import javafx.util.Callback;
-import javafx.scene.control.TableView;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
+import com.example.mobileorder.Item.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import java.util.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.StringConverter;
+import javafx.util.converter.IntegerStringConverter;
 
 public class maincontroller {
-    // ...
 
-    // Quantity Columnのセットアップ
     @FXML
     private TableView<Item> tableView;
-
-    // 以下、FXMLファイルで定義したTableViewの列をアノテーションでマッピング
     @FXML
     private TableColumn<Item, String> nameColumn;
     @FXML
-    private TableColumn<Item, Double> priceColumn;
+    private TableColumn<Item, Number> priceColumn;
     @FXML
     private TableColumn<Item, Integer> quantityColumn;
     @FXML
-    private TableColumn<Item, Void> addToCartColumn;
-    private void setupQuantityColumn() {
-        quantityColumn.setCellFactory(new Callback<TableColumn<Item, Integer>, TableCell<Item, Integer>>() {
-            @Override
-            public TableCell<Item, Integer> call(TableColumn<Item, Integer> param) {
-                return new TableCell<Item, Integer>() {
-                    @Override
-                    protected void updateItem(Integer item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            ChoiceBox<Integer> choiceBox = new ChoiceBox<>();
-                            choiceBox.getItems().addAll(1, 2, 3, 4, 5); // 例として5までの数量を設定
-                            // 現在のアイテムの数量を選択
-                            choiceBox.setValue(getItem());
-                            // 選択が変更された時のイベントハンドラ
-                            choiceBox.getSelectionModel().selectedItemProperty().addListener((obs, oldVal, newVal) -> {
-                                // ここで選択された数量を処理します
-                                Item Item = getTableView().getItems().get(getIndex());
-                                Item.setQuantity(newVal);
-                            });
-                            setGraphic(choiceBox);
-                        }
-                    }
-                };
-            }
-        });
-    }
+    private TableColumn<Item, String> addToCartColumn;
 
-    // Add To Cart Columnのセットアップ
-    private void setupAddToCartColumn() {
-        addToCartColumn.setCellFactory(new Callback<TableColumn<Item, Void>, TableCell<Item, Void>>() {
-            @Override
-            public TableCell<Item, Void> call(TableColumn<Item, Void> param) {
-                return new TableCell<Item, Void>() {
-                    private final Button addButton = new Button("カートに追加");
-
-                    {
-                        addButton.setOnAction(event -> {
-                            Item item = getTableView().getItems().get(getIndex());
-                            // ここでカートにアイテムを追加するロジックを実装します
-                            // addToCart(item);
-                        });
-                    }
-
-                    @Override
-                    protected void updateItem(Void item, boolean empty) {
-                        super.updateItem(item, empty);
-                        if (empty) {
-                            setGraphic(null);
-                        } else {
-                            setGraphic(addButton);
-                        }
-                    }
-                };
-            }
-        });
-    }
-
-    private void loadItems() {
-        // データベースからアイテムリストを取得
-        List<Item> itemList = connectionToDB.getItemsFromDatabase();
-
-        // アイテムリストをObservableListに変換
-        ObservableList<Item> observableItemList = FXCollections.observableArrayList(itemList);
-
-        // TableViewにアイテムリストをセット
-        tableView.setItems(observableItemList);
-    }
-
-
-    // initializeメソッドでこれらを呼び出します
-    @FXML
+    // 初期化メソッド
     public void initialize() {
-        // ...
+        // 商品名と価格に関するプロパティをセットアップ
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        // 数量選択のComboBoxをセットアップ
         setupQuantityColumn();
+
+        // カートに追加ボタンの設定をここに記述
         setupAddToCartColumn();
-        loadItems();
+
+        // 商品データの読み込み（ダミーまたはDBからの読み込み）
+        loadProducts();
     }
 
-    // loadItemsと他の必要なメソッドを含む
+    private void setupQuantityColumn() {
+        // 数量の選択肢を1から5までのリストとして生成
+        ObservableList<Integer> quantityOptions = FXCollections.observableArrayList(1, 2, 3, 4, 5);
+
+        // 数量の列をComboBoxとして設定
+        quantityColumn.setCellFactory(column -> new ComboBoxTableCell<>(new IntegerStringConverter(), quantityOptions));
+    }
+
+    private void setupAddToCartColumn() {
+        // カートに追加ボタンの列を設定（実装は省略）
+    }
+
+    private void loadProducts() {
+        // 商品データをロード（ダミーまたはDBからの読み込みを想定）
+    }
+
+    // その他のメソッド...
+
 }
 
+class ComboBoxTableCell<S, T> extends TableCell<S, T> {
+    private final ComboBox<T> comboBox;
+
+    public ComboBoxTableCell(StringConverter<T> converter, ObservableList<T> items) {
+        comboBox = new ComboBox<>(items);
+        comboBox.setConverter(converter);
+        comboBox.setEditable(true);
+
+        comboBox.valueProperty().addListener((obs, oldValue, newValue) -> {
+            if (isEditing()) {
+                commitEdit(newValue);
+            }
+        });
+    }
+
+    @Override
+    protected void updateItem(T item, boolean empty) {
+        super.updateItem(item, empty);
+
+        if (empty) {
+            setGraphic(null);
+        } else {
+            comboBox.setValue(item);
+            setGraphic(comboBox);
+        }
+    }
+
+    @Override
+    public void startEdit() {
+        super.startEdit();
+        comboBox.getSelectionModel().select(getItem());
+        setGraphic(comboBox);
+        comboBox.requestFocus();
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+        setGraphic(null);
+    }
+
+    @Override
+    public void commitEdit(T newValue) {
+        super.commitEdit(newValue);
+        setGraphic(null);
+    }
+}
